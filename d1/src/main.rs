978c8e444a276110b7dc6595ea392ad1f3afd9c7
Piotr Kochmański <kochmap@gmail.com>
2020-12-05 18:01:39 +0100

    d1p2

--- d1/src/main.rs
+++ d1/src/main.rs
@@ -3,21 +3,34 @@ use anyhow::Result;
 use std::fs;
 
 fn main() -> Result<()> {
-    let input = fs::read_to_string("input2")?;
+    let input = fs::read_to_string("input2.txt")?;
     let numbers: Vec<i32> = input
         .lines()
         .filter(|line| !line.is_empty())
         .map(|line| line.parse().unwrap())
         .collect();
-    let (n1, n2) = find_entries_sum(&numbers)?;
-    println!("n1,n2: {},{}; n1*n2={}", n1, n2, n1 * n2);
+    // let (n1, n2) = find_two_entries_sum_2020(&numbers)?;
+    // println!("n1,n2: {},{}; n1*n2={}", n1, n2, n1 * n2);
+    let (n1, n2, n3) = find_three_entries_sum_2020(&numbers)?;
+    println!("n1,n2,n3: {},{},{}; n1*n2*n3={}", n1, n2, n3, n1 * n2 * n3);
     Ok(())
 }
 
-fn find_entries_sum(numbers: &[i32]) -> Result<(i32, i32)> {
+fn find_two_entries_sum_2020(numbers: &[i32]) -> Result<(i32, i32)> {
     for (i, n1) in numbers.iter().enumerate() {
         if let Some(n2) = numbers[i + 1..].iter().find(|n2| n1 + *n2 == 2020) {
             return Ok((n1.clone(), n2.clone()));
+        }
+    }
+    Err(anyhow!("Not found"))
+}
+
+fn find_three_entries_sum_2020(numbers: &[i32]) -> Result<(i32, i32, i32)> {
+    for (i1, n1) in numbers.iter().enumerate() {
+        for (i2, n2) in numbers[i1 + 1..].iter().enumerate() {
+            if let Some(n3) = numbers[i2 + 1..].iter().find(|n3| n1 + *n2 + *n3 == 2020) {
+                return Ok((n1.clone(), n2.clone(), n3.clone()));
+            }
         }
     }
     Err(anyhow!("Not found"))
