4e7bae54b7b86527a155a807107b6689141aed68
Piotr Kochmański <kochmap@gmail.com>
2020-12-10 22:10:10 +0100

    d8p2

--- d8/src/main.rs
+++ d8/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let mut cpu = Cpu::new();
-    run_until_loop(&mut cpu, &input);
+    fix_program(&mut cpu, &input);
     println!("acc {}", cpu.acc)
 }
 
@@ -16,6 +16,44 @@ fn run_until_loop(cpu: &mut Cpu, input: &str) {
         }
         pc_log.push(cpu.pc);
         cpu.execute(&program);
+    }
+}
+
+fn run_until_exit_or_loop(cpu: &mut Cpu, program: &[Op]) -> bool {
+    let mut pc_log = Vec::new();
+    loop {
+        if cpu.pc >= program.len() {
+            return true;
+        }
+        if pc_log.contains(&cpu.pc) {
+            return false;
+        }
+        pc_log.push(cpu.pc);
+        cpu.execute(&program);
+    }
+}
+
+fn fix_program(cpu: &mut Cpu, input: &str) {
+    let mut program = Op::decode(input);
+    let op_to_check: Vec<usize> = program.iter().enumerate().filter_map(|(i, op)| match op {
+        Op::Jmp(_) => Some(i),
+        Op::Nop(_) => Some(i),
+        _ => None,
+    }).collect();
+    for i in op_to_check.iter() {
+        let removed = program.remove(*i);
+        let new_op = match removed {
+            Op::Jmp(n) => Op::Nop(n),
+            Op::Nop(n) => Op::Jmp(n),
+            _ => panic!("Wrong op to replace in {}", i),
+        };
+        program.insert(*i, new_op);
+        if run_until_exit_or_loop(cpu, &program) {
+            break;
+        }
+        cpu.reset();
+        program.remove(*i);
+        program.insert(*i, removed);
     }
 }
 
@@ -32,26 +70,36 @@ impl Cpu {
         }
     }
 
-    fn execute(&mut self, program: &[Op]) {
-        match program[self.pc] {
-            Op::Acc(n) => {
-                self.acc += n;
-                self.pc += 1;
+    fn execute(&mut self, program: &[Op]) -> bool {
+        if let Some(op) = program.get(self.pc) {
+            match op {
+                Op::Acc(n) => {
+                    self.acc += n;
+                    self.pc += 1;
+                }
+                Op::Jmp(n) => {
+                    self.pc = (self.pc as i64 + n) as usize;
+                }
+                Op::Nop(_) => {
+                    self.pc += 1;
+                }
             }
-            Op::Jmp(n) => {
-                self.pc = (self.pc as i64 + n) as usize;
-            }
-            Op::Nop => {
-                self.pc += 1;
-            }
+            true
+        } else {
+            false
         }
+    }
+
+    fn reset(&mut self) {
+        self.pc = 0;
+        self.acc = 0;
     }
 }
 
 enum Op {
     Acc(i64),
     Jmp(i64),
-    Nop,
+    Nop(i64),
 }
 
 impl Op {
@@ -61,10 +109,12 @@ impl Op {
 
     fn decode_line(line: &str) -> Op {
         let mut splitted = line.split_ascii_whitespace();
-        match splitted.next().unwrap() {
-            "acc" => Op::Acc(splitted.next().unwrap().parse::<i64>().unwrap()),
-            "jmp" => Op::Jmp(splitted.next().unwrap().parse::<i64>().unwrap()),
-            "nop" => Op::Nop,
+        let op_raw = splitted.next().unwrap();
+        let number = splitted.next().unwrap().parse::<i64>().unwrap();
+        match op_raw {
+            "acc" => Op::Acc(number),
+            "jmp" => Op::Jmp(number),
+            "nop" => Op::Nop(number),
             _ => panic!("Not known op {}", line),
         }
     }
@@ -92,5 +142,25 @@ acc +6
         run_until_loop(&mut cpu, input);
 
         assert_eq!(cpu.acc, 5);
+    }
+
+    #[test]
+    fn test_part2() {
+        let mut cpu = Cpu::new();
+        let input = r"
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+";
+
+        fix_program(&mut cpu, input);
+
+        assert_eq!(cpu.acc, 8);
     }
 }
